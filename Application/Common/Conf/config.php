2ef50f835ca3f29ddc58f776d6cedb8394c5ce6a
sunlong <2013855675@qq.com>
2019-04-20 12:11:26 +0800

    build(更新系统配置): 更新系统配置项

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 return array(
     //'配置项'=>'配置值'
 
@@ -6,9 +6,9 @@ return array(
     'DB_CHARSET' => 'utf8', // 数据库字符编码
     'DB_TYPE' => 'mysql', // 数据库类型
     'DB_HOST' => 'mysql', // 主机名称
-    'DB_NAME' => 'share',
-    'DB_USER' => 'share', // 数据库管理员用户名
-    'DB_PWD' => 'share', // 数据库管理员密码
+    'DB_NAME' => getenv('DB_DATABASE'),
+    'DB_USER' => getenv('DB_USERNAME'), // 数据库管理员用户名
+    'DB_PWD' => getenv('DB_PASSWORD'), // 数据库管理员密码
     'DB_PREFIX' => '', // 数据库前缀
     'SHOW_PAGE_TRACE' => true, // 显示页面Trace信息
 
